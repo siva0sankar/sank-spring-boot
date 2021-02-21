8270c68f0df15152377332458ebbc913d926755c
Sivasankar <siva0sankar@gmail.com>
2021-02-21 19:41:52 +0530

    re-created

---
+++ src/main/java/com/siva/sank/SankApplication.java
@@ -0,0 +1,13 @@
+package com.siva.sank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SankApplication.class, args);
+    }
+
+}
